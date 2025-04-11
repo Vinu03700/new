6766fc8cbc616f619551e23fa7983283b1415476
vinu033 <vinaykalasannavar124@gmail.com>
2025-04-11 10:13:47 +0530

    Create largenum.java

---
+++ largenum.java
@@ -0,0 +1,13 @@
+public class FindLargest {
+
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 8;
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is the larger number.");
+        } else {
+            System.out.println(num2 + " is the larger number.");
+        }
+    }
+}
